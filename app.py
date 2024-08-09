f22ae05200fa5869ff9fcd29567ed204b576ed73
Chethan M <020chethan@gmail.com>
2024-08-09 17:57:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+
+load_dotenv()  # Load all the environment variables
+
+# Configure Google Gemini API key
+genai.configure(api_key="AIzaSyC3_Fb1Bd-WGenvT8BUMNuWQyByzR3-VjY")
+
+prompt = """You are a YouTube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
+
+# Function to extract transcript details from YouTube video
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id = youtube_video_url.split("=")[1]
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+        return transcript
+    except Exception as e:
+        raise e
+
+# Function to generate content based on the prompt from Google Gemini Pro
+def generate_gemini_content(transcript_text, prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt + transcript_text)
+    return response.text
+
+# Add custom CSS to style the background
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(135deg, #FF4F4F, #000000); /* Vibrant white to deep black gradient */
+        color: #FFFFFF; /* White text for better contrast */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Get Detailed Notes"):
+    transcript_text = extract_transcript_details(youtube_link)
+    if transcript_text:
+        summary = generate_gemini_content(transcript_text, prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
